9bd16c2aa15197817d920f250b6558d4ac550c7c
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-16 22:33:50 +0200

    ok
    ok

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace T05Ejercicio12
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] array = new double[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Añada el " + (i + 1) + "º valor: ");
+                double valor = Convert.ToDouble(Console.ReadLine());
+                array[i] = valor;
+            }
+            //Utilizo el método Average de System.Linq Namespace para el promedio
+            Console.WriteLine("La media de todos los valores es: " + array.Average());
+
+        }
+    }
+}
